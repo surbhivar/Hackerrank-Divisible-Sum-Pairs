bc47807ae459f7f2f78dfd737777d4352bf4f769
Surbhi Varshney <varshneysurbhi@gmail.com>
2020-06-15 20:37:19 +0530

    Create divsum.cpp

---
+++ #include <bits/divsum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+int i, j ,k ,n, count=0;
+
+cin >> n >> k;
+int array[n];
+
+for(i = 0; i < n ; i++)
+{
+cin >> array[i];
+}
+
+for(i = 0; i < n ; i++)
+{
+for(j = 0; j < n ; j++)
+{
+if(i < j)
+{
+if((array[i] + array[j]) % k == 0)
+count++;
+}
+}
+}
+cout<<count;
+
+return 0;
+
+}
